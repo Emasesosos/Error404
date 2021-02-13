9941d2168b37b5100ad1f9c11bafe7ad88a66eda
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2021-02-13 03:14:25 -0600

    App fin

--- page404-app/src/components/Page404/Page404.js
+++ page404-app/src/components/Page404/Page404.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import logo from './../../../src/logo.svg';
+import crow from './../../img/Scarecrow.png';
 
 export const Page404 = () => {
     return (
-        <div className="mainbox">
-            <div className="oops">
-                <h2>Oops! Page not found.</h2>
+        <div className="page404__container">
+            <div className="page404__name">
+                <p>404 NOT FOUND</p>
             </div>
-            <div className="number">
-                <h1>4</h1>
-                <h1>0</h1>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>4</h1>
+            <div className="page404__main">
+                <div className="page404__img">
+                    <img src={crow} alt={crow}/>
+                </div>
+                <div className="page404__info">
+                    <div className="page404__title">
+                        <p>I have bad news for you</p>
+                    </div>
+                    <div className="page404__description">
+                        <p>The page you are looking for might be removed or is temporarily unavailable</p>
+                    </div>
+                    <div className="page404__back">
+                        <a href="/">BACK TO HOMEPAGE</a>
+                    </div>
+                </div>
             </div>
-            <div className="msg">
-                <p>We can´t find the page you´re looking for.</p>
-                <a href="https://www.youtube.com/">Go back home</a>
-            </div>
+            <footer>
+                <div className="page404__footer">
+                    <a href="https://github.com/Emasesosos" target="_blank"rel="noopener noreferrer">Emasesosos</a>
+                    <p>@</p>
+                    <a href="https://devchallenges.io/" target="_blank" rel="noopener noreferrer">DevChallenges.io</a>
+                </div>
+            </footer>
         </div>
     );
 };
--- page404-app/src/index.js
+++ page404-app/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ErrorApp } from './ErrorApp';
 import './styles/styles.scss';
-//import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <ErrorApp />,
